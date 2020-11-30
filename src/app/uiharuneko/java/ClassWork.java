9565020f8694ab53d69d07770d63c2bfd85bb67c
UiharuNeko <uiharuneko@gmail.com>
2020-11-30 19:15:06 +0800

    First edition

---
+++ src/app/uiharuneko/java/ClassWork.java
@@ -0,0 +1,60 @@
+package app.uiharuneko.java;
+
+import java.util.Scanner;
+
+public class ClassWork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("两位数计算器");
+        boolean flag = true;
+        do{
+            mainWay();
+            System.out.println("如果要继续计算，请按1");
+            int redo = sc.nextInt();
+            if(redo != 1 ){
+                flag = false;
+            }
+        }while(flag);
+    }
+    public static void mainWay(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入要执行的功能");
+        System.out.println("1为相加，2为相减，3为相乘，4为相除");
+        int feature = sc.nextInt();
+        System.out.println("请输入第一位数");
+        double first = sc.nextDouble();
+        System.out.println("请输入第二位数");
+        double second = sc.nextDouble();
+        double calc = 0;
+        switch(feature) {
+            case 1:
+                calc = add(first, second);
+                break;
+            case 2:
+                calc = sub(first, second);
+                break;
+            case 3:
+                calc = mcl(first, second);
+                break;
+            case 4:
+                calc = div(first, second);
+                break;
+            default:
+                System.out.println("请重新输入");
+                break;
+        }
+        System.out.println(calc);
+    }
+    public static double add(double first,double second){
+        return first+second;
+        }
+    public static double sub(double first,double second){
+        return first-second;
+    }
+    public static double mcl(double first,double second){
+        return first*second;
+    }
+    public static double div(double first,double second){
+        return first/second;
+    }
+}
